2fa3acca004bb3a5747c29e0aed6cb70e5cb4996
Sua_Jimenez_HKKD <sua_jimenez@hakkoda.io>
2024-08-20 10:12:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-Less minimal app.py code here:
-https://dash.plotly.com/deployment
-+ 
-correct invocation line here:
-https://community.plotly.com/t/error-with-gunicorn-application-object-must-be-callable/31397 
-
-test invoke with: (but will end when your ec2 terminal session ends, resets, etc.)
-    $ gunicorn app:server --bind=0.0.0.0:8501
-
-For persistent production you will need: (end with kill process number) 
-    (ENV)$ nohup gunicorn app:server --bind=0.0.0.0:8501 &
-    or
-    (ENV)$ screen gunicorn app:server --bind=0.0.0.0:8501 &
-
 
 # Import your Python Libraries (you may need boto3 for AWS)
 from dash import Dash, dcc, html, Input, Output

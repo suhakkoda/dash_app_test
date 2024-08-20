042304c727ac8b82129c14180cb0e7713d148fc6
Sua_Jimenez_HKKD <sua_jimenez@hakkoda.io>
2024-08-20 09:58:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from codecarbon.viz.carbonboard import render_app
-import pandas as pd
+Less minimal app.py code here:
+https://dash.plotly.com/deployment
++ 
+correct invocation line here:
+https://community.plotly.com/t/error-with-gunicorn-application-object-must-be-callable/31397 
 
-df = pd.read_csv("emissions.csv")
+test invoke with: (but will end when your ec2 terminal session ends, resets, etc.)
+    $ gunicorn app:server --bind=0.0.0.0:8050
 
-app = render_app(df)
+For persistent production you will need: (end with kill process number) 
+    (ENV)$ nohup gunicorn app:server --bind=0.0.0.0:8050 &
+    or
+    (ENV)$ screen gunicorn app:server --bind=0.0.0.0:8050 &
 
+
+# Import your Python Libraries (you may need boto3 for AWS)
+from dash import Dash, dcc, html, Input, Output
+import os
+
+# Connects Dash -> Flask (for wsgi/gunicorn production server)
+app = Dash(__name__)
+
+# needed for gunicorn/wsgi to connect
 server = app.server
 
-if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8501, debug=True)
+# Your visualization code goes here etc.
+app.layout = html.Div([
+    html.H2('Hello World')
+])
+
+if __name__ == '__main__':
+    app.run_server(host= '0.0.0.0', port=8050)
+
